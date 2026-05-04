3cfab3102378ff9e5ae320b1fcef1777d9d7a751
Ilya19951110 <ilyasachenko@gmail.com>
2026-05-04 12:31:38 +0300

    feat: add revenue layout with 3/1 split 16

--- frontend/features/trends/panel.py
+++ frontend/features/trends/panel.py
@@ -53,7 +53,7 @@ class Panel:
         role = self.user.get('role')
         drawer = get_drawer(role=role)
         await get_header(drawer=drawer, role=role, request=self.request)
-        with ui.element('div').classes('w-full max-w-[2000px] mx-auto px-7 mt-3') as self.container:
+        with ui.element('div').classes('w-full max-w-[2000px] mx-auto px-3 mt-3') as self.container:
             await self.render_content()
 
     async def render_content(self):
