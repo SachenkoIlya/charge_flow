367b254920123aff47c64c7a239ef778bcefdb02
Ilya19951110 <ilyasachenko@gmail.com>
2026-05-06 15:24:56 +0300

    refactor(trends.panel): add filters 28

--- frontend/utils/utils.py
+++ frontend/utils/utils.py
@@ -2,16 +2,16 @@ from .normalize_phone import NormalizePhone
 from .current_user import CurrentUser
 from core.logger.logger import make_logger
 from frontend.utils.decorators import Auth
-from dataclasses import  dataclass
+from dataclasses import  dataclass, field
 
 @dataclass
 class Decorators:
     logger: make_logger
     current_user: "CurrentUser"
+    auth: "Auth" = field(init=False)
     
-    auth = Auth(current_user, logger)
-
-
+    def __post_init__(self):
+        self.auth = Auth(self.current_user, self.logger)
 
 
 class Utils:
@@ -21,3 +21,6 @@ class Utils:
     decorators =Decorators(current_user, logger)
     
 utils = Utils()
+
+
+
