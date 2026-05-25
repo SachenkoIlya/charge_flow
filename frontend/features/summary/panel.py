a76ae7dd7d13f505ccbb42f59ab24c00016eb87a
Ilya19951110 <ilyasachenko@gmail.com>
2026-05-25 12:22:38 +0300

    wip 13

--- frontend/features/summary/panel.py
+++ frontend/features/summary/panel.py
@@ -27,7 +27,17 @@ class Panel(BasePanel):
         role = self.user.get('role')
 
         with ui.element('div').classes(
-            'w-screen min-h-screen flex bg-gradient-to-br from-[#050b12] via-[#08111b] to-[#0b1724] text-white overflow-hidden'
+            """
+                w-screen
+                h-screen
+                flex
+                bg-gradient-to-br
+                from-[#050b12]
+                via-[#08111b]
+                to-[#0b1724]
+                text-white
+                overflow-hidden
+            """
         ):
             render_sidebar(role=role)
 
