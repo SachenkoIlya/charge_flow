19623f896b5b9ea9eb09b63ab71449f3f5a5a9bc
Ilya19951110 <ilyasachenko@gmail.com>
2026-05-25 15:07:20 +0300

    wip 21

--- frontend/components/metric_card.py
+++ frontend/components/metric_card.py
@@ -1,30 +1,35 @@
 from nicegui import ui
 
-
 def metric_card(metric: dict):
     with ui.card().classes(
         """
-            bg-[#101923]/90
-            border border-[#1f2937]
-            rounded-xl
-            shadow-xl
-            p-3
-            text-white
-            h-[105px]
+        bg-[#101923]/90
+        border border-[#1f2937]
+        rounded-xl
+        shadow-xl
+        p-3
+        text-white
+        h-[105px]
+        overflow-hidden
         """
     ):
+
         with ui.row().classes('items-start gap-3'):
+
             with ui.element('div').classes(
                 f"""
-                    {metric["icon_bg"]}
-                    w-10 h-10
-                    rounded-lg
-                    flex items-center justify-center
+                {metric["icon_bg"]}
+                w-10 h-10
+                rounded-lg
+                flex items-center justify-center
                 """
             ):
-                ui.icon(metric['icon']).classes('text-white text-lg')
-            
-            with ui.column().classes('gap-1'):
+                ui.icon(metric['icon']).classes(
+                    'text-white text-lg'
+                )
+
+            with ui.column().classes('gap-1 flex-1'):
+
                 ui.label(metric['title']).classes(
                     'text-sm text-white'
                 )
@@ -32,11 +37,11 @@ def metric_card(metric: dict):
                 ui.label(metric['subtitle']).classes(
                     'text-xs text-gray-400'
                 )
-            ui.label(metric['value']).classes(
-                'text-xl font-bold mt-3'
-            )
 
-            ui.label(metric['delta']).classes(
-                'text-sm text-green-400 mt-1'
-            )
-     
+                ui.label(metric['value']).classes(
+                    f"{metric.get('value_class', 'text-xl')} font-bold mt-2 whitespace-normal break-words leading-tight"
+                )
+
+                ui.label(metric['delta']).classes(
+                    'text-xs text-green-400 mt-1'
+                )
