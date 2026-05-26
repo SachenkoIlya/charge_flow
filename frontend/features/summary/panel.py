7c73e3dd80bcb1f91011153fe2a6fc7ca20fea32
Ilya19951110 <ilyasachenko@gmail.com>
2026-05-26 12:46:13 +0300

    rollback frontend 2

--- frontend/features/summary/panel.py
+++ frontend/features/summary/panel.py
@@ -66,7 +66,7 @@ class Panel(BasePanel):
         style: str = None
         label_aggre: str = None
         # with ui.element('div').classes('w-full max-w-[1700px]'):
-        with ui.element('div').style('zoom: 0.9'):
+        with ui.element('div').style('zoom: 0.8'):
             await render_title(
                 label='Общая сводка по сети',
                 label_aggre='Executive Dashboard',
