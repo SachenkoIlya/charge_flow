7a5e28175d153ac5e55d0117843480778c035a60
Ilya19951110 <ilyasachenko@gmail.com>
2026-05-27 13:36:18 +0300

    add ui toggle in render title

--- frontend/components/render_title.py
+++ frontend/components/render_title.py
@@ -20,9 +20,14 @@ async def render_title(
             ui.label(label_aggre).classes(
                 'text-sm text-gray-400 mt-1'
             )
-
-        await get_calendar(
-            page_key=page_key,
-            on_change_date=on_date_change,
-        )
+        if page_key in {'finance'}:
+            ui.toggle(
+                ['6 мес', '1 год', 'Все'],
+                value='6 мес'
+            )
+        else:
+            await get_calendar(
+                page_key=page_key,
+                on_change_date=on_date_change,
+            )
     
