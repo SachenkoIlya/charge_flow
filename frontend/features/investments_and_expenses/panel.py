5686c3724798f7a42d0a39b8220b3f585ef8afba
Ilya19951110 <ilyasachenko@gmail.com>
2026-06-01 10:22:41 +0300

    feat(investments): add investments and expenses page

---
+++ frontend/features/investments_and_expenses/panel.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from fastapi import Request
+from frontend.components.drawer import render_sidebar
+from frontend.components.render_title import render_title
+from frontend.features.base.panel import BasePanel
+from nicegui import ui
+
+
+@dataclass
+class Panel(BasePanel):
+    user: dict
+    request: Request
+    endpoints_name: str = 'investments_and_expenses'
+    page_key = 'investments_and_expenses'
+
+    
+    async def render(self):
+        self.apply_filters()
+        # loaded = await self.load_data()
+        # if not loaded:
+        #     return
+        
+        role = self.user.get('role')
+
+        with ui.element('div').classes(
+            """
+                w-screen
+                h-screen
+                flex
+                bg-gradient-to-br
+                from-[#050b12]
+                via-[#08111b]
+                to-[#0b1724]
+                text-white
+                overflow-hidden
+            """
+        ):
+            render_sidebar(role=role)
+
+            with ui.element('main').classes(
+                        """
+                            flex-1
+                            h-screen
+                            overflow-hidden
+                            px-10
+                            py-2
+                        """
+                ) as self.container:
+                    await self.render_content()
+
+
+    async def render_content(self):
+         with ui.element('div').style('zoom: 1'):
+            await render_title(
+                label='CAPEX & OPEX',
+                label_aggre='Инвестиции и операционные расходы',
+                page_key=self.page_key,
+                on_date_change=self.on_date_change 
+            )
