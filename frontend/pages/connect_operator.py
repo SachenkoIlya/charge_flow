b18c69d035ebbea233186e50b04e7c871f8077fe
Ilya19951110 <ilyasachenko@gmail.com>
2026-06-01 10:39:36 +0300

    feat(connect operator): wip 4

--- frontend/pages/connect_operator.py
+++ frontend/pages/connect_operator.py
@@ -4,7 +4,7 @@ from fastapi import Request
 from frontend.components.setup_theme import setup_theme
 from frontend.utils.utils import utils
 from frontend.utils.config import screen_background
-
+from core.logger.logger import logger
 
 @ui.page('/operator')
 @utils.decorators.auth.require_auth
@@ -12,4 +12,6 @@ async def connect_operator_page(request: Request):
     setup_theme()
     user = app.storage.user.get('user')
     ui.query('body').classes(screen_background)
+    logger.debug(f"page: connect_operator".upper())
+    logger.debug(f"{user}: {user}")
     await ConnectOperatorForm(user=user, request=request).render()
