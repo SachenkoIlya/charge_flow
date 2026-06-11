996d6cf2fc0bc60900035ee705714f8392ae6282
Ilya19951110 <ilyasachenko@gmail.com>
2026-06-11 21:33:16 +0300

    feat(summary): complete dashboard summary endpoint 46

--- frontend/features/summary/render/tables_section.py
+++ frontend/features/summary/render/tables_section.py
@@ -7,7 +7,11 @@ from frontend.components.render_top_table import  render_top_table
 
 def render_tables_section(rows: list[dict], reversed_rows:list[dict]):
     with ui.grid(columns=2).classes(
-        'w-full gap-4'
+        """
+        w-full
+        h-[70vh]
+        gap-4
+        """
     ):
         render_top_table('Топ-5 станций по выручке', 'text-green-400', rows)
         render_top_table('Топ-5 худших станций по выручке', 'text-orange-400', reversed_rows)
