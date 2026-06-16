8e5881956052c2388571368d003181a4b5c65ef4
Ilya19951110 <ilyasachenko@gmail.com>
2026-06-16 18:36:41 +0300

    changed the input data schemas and added expense_date 6

--- frontend/features/investments_and_expenses/render/form.py
+++ frontend/features/investments_and_expenses/render/form.py
@@ -241,7 +241,7 @@ async def render_form(data: dict[str, list], request: Request, mode:str = 'opex'
 
                 with inputs['expense_date'].add_slot('append'):
                     ui.icon('event').classes('cursor-pointer')
-                with ui.menu().props('no-parent-event anchor=bottom left self=top left') as menu:
+                with ui.menu().props('anchor=bottom left self=top left') as menu:
                     ui.date(
                         value=datetime.now().strftime('%Y-%m-%d'),
                         on_change=lambda e: (
