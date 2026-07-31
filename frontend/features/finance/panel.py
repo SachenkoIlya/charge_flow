65c1e497aca74ddbf2566631cff191a2513eac42
Ilya19951110 <ilyasachenko@gmail.com>
2026-07-31 13:00:23 +0300

    finance panel: wip 12

--- frontend/features/finance/panel.py
+++ frontend/features/finance/panel.py
@@ -102,7 +102,7 @@ class Panel(BasePanel):
             render_finance_charts(
                 # cash_flow_history=charts['cash_flow_history'],
                 # break_even=BREAK_EVEN_METRICS,
-                cost_structure=charts['network_cost_structure'],
+                # cost_structure=charts['network_cost_structure'],
                 pnl_table=PNL_ROWS
             )
             
