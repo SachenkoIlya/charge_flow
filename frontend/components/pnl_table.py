56759b73481380f03683eee38d32ee99523d43be
Ilya19951110 <ilyasachenko@gmail.com>
2026-07-31 12:58:40 +0300

    finance panel: wip 11

--- frontend/components/pnl_table.py
+++ frontend/components/pnl_table.py
@@ -1,10 +1,10 @@
 from nicegui import ui
 
 def render_pnl_table(rows: list[dict], height:int=600):
+        # h-[{height}px]
        with ui.card().classes(
         f'''
             w-full
-            h-[{height}px]
             bg-[#101923]/90
             border border-[#1f2937]
             rounded-xl
